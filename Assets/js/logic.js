f7de523133acb1b66232918c8792843c6e229f9e
Lex Crawford <lexcraw4d@gmail.com>
2022-05-21 16:55:56 -0400

    added feature to hide when quiz starts

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,22 +1,26 @@
 let time = 10;
+let currentIndex = 0;
 let startButtonEl = document.getElementById('start-btn');
 let timerEl = document.getElementById('timer');
+let questionEl = document.getElementById('questions');
+var gameIntroEl = document.getElementById('game-intro');
 var timerId;
-startButtonEl.addEventListener('click', startQuiz);
-
 
 
 function startQuiz(){
+    //hide main game introduction
+    gameIntroEl.setAttribute('class','hide');
     //start timer
-    timerId = setInterval(counter, 1000)
-
-    
+    timerId = setInterval(counter, 1000);
 }
+function displayQuestions(){
 
-
+}
 function counter(){
     timerEl.textContent = time;
     if(time > 0) {
         time--
     }
 };
+
+startButtonEl.addEventListener('click', startQuiz);
