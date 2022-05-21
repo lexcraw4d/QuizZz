87de59aa04ae0c8fe4c7c76f630a9f5f84f10f28
Lex Crawford <lexcraw4d@gmail.com>
2022-05-21 16:18:31 -0400

    added timer counter to start on gameplay

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,11 +1,22 @@
-let intialTime = 60;
+let time = 10;
 let startButtonEl = document.getElementById('start-btn');
+let timerEl = document.getElementById('timer');
+var timerId;
+startButtonEl.addEventListener('click', startQuiz);
 
 
-startButtonEl.addEventListener('click', startCountdown);
 
-function startCountdown(){
-    alert('you clicked me!')
+function startQuiz(){
+    //start timer
+    timerId = setInterval(counter, 1000)
+
+    
+}
+
+
+function counter(){
+    timerEl.textContent = time;
+    if(time > 0) {
+        time--
+    }
 };
-
-
