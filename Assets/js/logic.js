5f47b4aead7f3fb11b05d65aff81e7c3251a34d3
Lex Crawford <lexcraw4d@gmail.com>
2022-05-21 15:41:06 -0400

    added start button and timer

---
+++ Assets/js/logic.js
@@ -0,0 +1,11 @@
+let intialTime = 60;
+let startButtonEl = document.getElementById('start-btn');
+
+
+startButtonEl.addEventListener('click', startCountdown);
+
+function startCountdown(){
+    alert('you clicked me!')
+};
+
+
