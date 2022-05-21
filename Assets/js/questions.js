8ec4dff9f48c699a6bf622aeaec86e6830710e23
Lex Crawford <lexcraw4d@gmail.com>
2022-05-21 16:27:29 -0400

    added dummy data questions

---
+++ Assets/js/questions.js
@@ -0,0 +1,18 @@
+
+let questions = [
+{
+    question: "How old am I",
+    choices: [1, 34, 33, 2],
+    answer: 33
+},
+{
+    question: "Who is my favorite pet",
+    choices: ["Scout", "Acorn", "Meadow", "Florence", "Tucker", "ALL!"],
+    answer: "ALL!"
+},
+{
+    question: "What is my favorite team?",
+    choices: ["Gamecocks", "Clemson", "Georgia", "Oregon"],
+    answer:"Gamecocks"
+}
+]
