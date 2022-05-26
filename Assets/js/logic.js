6d439a01f15caccb96d050307142570cf99a2043
Lex Crawford <lexcraw4d@gmail.com>
2022-05-25 20:45:42 -0400

    added click function and values show on click for comparison later

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -20,7 +20,6 @@ function displayQuestions(){
     //set attributes to removeAttribut('class', 'hide') on questionsEl and 
     //iterate over questions at each index
     //display question and choices at each index given
-    //
     //increase currentindex on button click to go to the next question --> current index i++
     questionEl.removeAttribute('class', 'hide');
     choicesEl.removeAttribute('class', 'hide');
@@ -28,23 +27,35 @@ function displayQuestions(){
     // choicesEl.textContent = "Answers will go here BaZiNgA!";
 
     //Assigning the index of current question and choices to respective containers
-    let currentQuestion = questions[currentIndex].question;
-    let currentChoices = questions[currentIndex].choices;
+    let currentQuestion = questions[currentIndex];
 
-    questionEl.textContent = currentQuestion;
+    questionEl.textContent = currentQuestion.question;
 
     //later change to append btns
-    choicesEl.textContent = currentChoices;
-
     //create a button for each choice using for each choice on displayed question
-    currentChoices.forEach(choice => {
-        let choiceBtns = document.createElement("button");
+    currentQuestion.choices.forEach(function(choice, i) {
         console.log(choice)
-        choiceBtns.textContent = currentChoices;
-        choicesEl.append(choiceBtns)
-    });
+        let choiceBtns = document.createElement("button");
 
+        choiceBtns.setAttribute("value",choice)
+        
+        choiceBtns.textContent = choice;
+        
+        //display on page
+        choiceBtns.onclick = questionClick;
+        choicesEl.appendChild(choiceBtns)
+       
+    }
+    )
 }
+function questionClick (event) {
+    console.log(event.target.value)
+  //if event.target.val != question[currentindex] val then wrong
+  //else correct bazinga! 
+  //time penality 
+  //sounds eventually added
+}
+
 function counter(){
     timerEl.textContent = time;
     if(time > 0) {
--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -1,12 +1,12 @@
 
 let questions = [
 {
-    question: "How old am I",
+    question: "How old am I?",
     choices: [1, 34, 33, 2],
     answer: 33
 },
 {
-    question: "Who is my favorite pet",
+    question: "Who is my favorite pet?",
     choices: ["Scout", "Acorn", "Meadow", "Florence", "Tucker", "ALL!"],
     answer: "ALL!"
 },
